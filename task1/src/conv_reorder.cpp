40d4ad289dad5235df8aa9c8c8ea929b526a947a
grajs2803 <grajs280303@gmail.com>
2026-08-26 11:15:11 +0530

    2d conv loop reorder commit

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -3,8 +3,38 @@
 
 #include "convolution.h"
 
-void conv_reorder(const float* in, float* out, const float* ker,
-                  int H, int W, int K) {
+void conv_reorder(const float *in, float *out, const float *ker,
+                  int H, int W, int K)
+{
     // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+    conv_reorder2(in, out, ker, H, W, K);
+}
+void conv_reorder2(const float *in, float *out, const float *ker,
+                 int H, int W, int K)
+{
+    const int p = K / 2;
+    const int in_stride = W + 2 * p; // padded row stride
+    int runs = H - K + 1;
+
+    for (int  j = 0; j < H; j++)
+    {
+        for(int i=0;i<W;i++){
+            out[j*W+i]=0.0f;
+        }
+    }
+    
+    for (int ky = 0; ky < K; ky++)
+    {
+        for (int kx = 0; kx < K; kx++)
+        {
+            float kerv = ker[ky * K + kx];
+            for (int oy = 0; oy < H; oy++)
+            {
+                for (int ox = 0; ox < W; ox++)
+                {
+                    out[oy * W + ox] += in[(oy + ky) * in_stride + (ox + kx)] * kerv;
+                }
+            }
+        }
+    }
 }
