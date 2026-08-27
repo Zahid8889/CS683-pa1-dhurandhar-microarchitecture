7bb2c6d24c20978f969c942eb45310273f0ff344
Ankit Banerjee <banerjeeankitmail@gmail.com>
2026-08-27 19:32:45 +0530

    Fixes and updating the correct file this time, reorder done

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -5,6 +5,28 @@
 
 void conv_reorder(const float* in, float* out, const float* ker,
                   int H, int W, int K) {
-    // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+
+    const int p = K/2;
+    const int in_stride = W+2*p;  
+
+    //Initialize the result with 0s (we will iteratively add in each index)- Ankit
+    for(int oy=0 ; oy<H ; oy++){
+        for(int ox = 0 ; ox<W ; ox++){
+            out[oy*W + ox] = 0.0f;
+        }
+    }
+
+    //reorder loop: ky->kx->oy->ox - Ankit
+    for(int ky=0 ; ky<H ; ky++){
+        for(int kx=0 ; kx<W ; kx++){
+
+            float w = ker[ky*K + kx];
+            for(int oy=0 ; oy<H ; oy++){
+                for(int ox = 0 ; ox<W ; ox++){
+                    out[oy*W + ox] += (in[(oy + ky) *in_stride + (ox + kx) ] * w);
+                }
+            }
+        }
+    }
+    
 }
