0fba9284f2378e238a1157266f7d244da8235cf0
Zahid8889 <zahidthelord@gmail.com>
2026-08-29 05:02:09 +0000

    Task1 Simd 2-4x sppedup achieved

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -6,5 +6,30 @@
 void conv_simd(const float* in, float* out, const float* ker,
                int H, int W, int K) {
     // TODO(student): replace this placeholder with your AVX2 implementation.
-    conv_naive(in, out, ker, H, W, K);
+    // conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+    for (int oy = 0; oy < H; ++oy) 
+        for (int ox = 0; ox < W; ++ox)
+                    out[oy * W + ox]= 0.0f;
+                    
+    for (int ky = 0; ky < K; ++ky) {
+        for (int oy = 0; oy < H; ++oy) {
+            for (int kx = 0; kx < K; ++kx) {
+                int ox = 0;
+                const __m512 w = _mm512_set1_ps(ker[ky * K + kx]);
+                for (; ox+15 < W; ox+=16) {
+                    __m512 reg_a = _mm512_loadu_ps((in + ((oy + ky) * in_stride + (ox + kx))));
+                    __m512 reg_prod = _mm512_mul_ps(reg_a, w);
+                    __m512 reg_out = _mm512_loadu_ps(out + (oy * W + ox));
+                    __m512 reg_result = _mm512_add_ps(reg_out, reg_prod);
+                    _mm512_storeu_ps((out + oy * W + ox), reg_result);
+                }
+                for(;ox<W;ox++){
+                    out[oy * W + ox]+= in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+                }
+            }
+        }
+    }
+    
 }
