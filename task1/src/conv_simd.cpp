8f2cfdc1d88c31ada77b7e211db3195cbefbfe4f
Ankit Banerjee <banerjeeankitmail@gmail.com>
2026-09-04 20:11:25 +0530

    initialization of output taken inside main loop

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -8,30 +8,35 @@ void conv_simd(const float* in, float* out, const float* ker,
     const int p = K/2;
     const int in_stride = W+2*p;
 
-    //Initialize
-    for(int oy=0 ; oy<H ; oy++){
-        for(int ox=0 ; ox<W ; ox++){
-            out[oy*W+ox] = 0.0f;
-        }
-    }
-
     for(int ky=0 ; ky<K ; ky++){
         for(int kx=0 ; kx<K ; kx++){
 
             float w = ker[ky*K + kx];
 
-            __m256 weight = _mm256_set1_ps(w); // a vectos of 8 floats using a single float value
+            const __m256 weight = _mm256_set1_ps(w); // a vectos of 8 floats using a single float value
             
             for(int oy=0 ; oy<H ; oy++){
                 for(int ox=0 ; ox<W ; ox+=8){
                     
                     __m256 input = _mm256_loadu_ps(&in[(oy + ky) * in_stride + (ox + kx)]); // loads the input from memory to a vertor of 8 floats
-                     
-                    __m256 output = _mm256_loadu_ps(&out[oy*W+ox]); 
+                    
+                    // First kernel element: initialize output
+                    if (ky == 0 && kx == 0) {
+                        __m256 output =
+                            _mm256_mul_ps(input, weight);
 
-                    output = _mm256_fmadd_ps(input, weight, output);
+                        _mm256_storeu_ps(
+                            &out[oy * W + ox],
+                            output
+                        );
+                    }else{
 
-                    _mm256_storeu_ps(&out[oy*W+ox], output);
+                        __m256 output = _mm256_loadu_ps(&out[oy*W+ox]); 
+
+                        output = _mm256_fmadd_ps(input, weight, output);
+
+                        _mm256_storeu_ps(&out[oy*W+ox], output);
+                    }
 
                 }
             }
