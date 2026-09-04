d7077ba5788d5570db848387b4f1c8cdf4c5003a
Ankit Banerjee <banerjeeankitmail@gmail.com>
2026-09-04 19:05:06 +0530

    software prefetching added to task2-A, yet to test

--- task2/src/matmul_prefetch.cpp
+++ task2/src/matmul_prefetch.cpp
@@ -8,7 +8,7 @@ void matmul_prefetch(const float* A, const float* B, float* C,
                      int M, int N, int K, int lda, int ldb, int ldc) {
     
     const int BLOCK = 64;
-    const int PREFETCH = 64;
+    const int PREFETCH_DIS = 64;
     //Initialize output
     for(int i=0 ; i<M; i++){
         for(int j=0 ; j<N ; j++){
@@ -35,6 +35,23 @@ void matmul_prefetch(const float* A, const float* B, float* C,
                         float acc = C[static_cast<long>(i)*ldc + j];
 
                         for(int p=pp ; p<p_end ; p++){
+                            // Prefetch future data
+                            if (p + PREFETCH_DIS < p_end) {
+
+                                _mm_prefetch(
+                                    reinterpret_cast<const char*>(
+                                        a + p + PREFETCH_DIS
+                                    ),
+                                    _MM_HINT_T0
+                                );
+
+                                _mm_prefetch(
+                                    reinterpret_cast<const char*>(
+                                        b + p + PREFETCH_DIS
+                                    ),
+                                    _MM_HINT_T0
+                                );
+                            }
                             acc += a[p] * b[p];
                         }
 
