53e82434bf2af5c2b5230fa0cbed71c61b83d06c
Ankit Banerjee <banerjeeankitmail@gmail.com>
2026-09-04 16:34:59 +0530

    cache blocking added, not tested

--- task2/src/matmul_prefetch.cpp
+++ task2/src/matmul_prefetch.cpp
@@ -6,7 +6,46 @@
 
 void matmul_prefetch(const float* A, const float* B, float* C,
                      int M, int N, int K, int lda, int ldb, int ldc) {
-    // TODO(student): replace this placeholder with your cache-blocked SIMD + prefetch
-    // implementation.
-    matmul_naive(A, B, C, M, N, K, lda, ldb, ldc);
+    
+    const int BLOCK = 64;
+
+    //Initialize output
+    for(int i=0 ; i<N; i++){
+        for(int j=0 ; j<K ; j++){
+            C[static_cast<long>(i)*lda+j] = 0.0f;
+        }
+    }
+
+    //Blocked matrix multiplication
+    for(int ii=0; ii<M ; ii+=BLOCK){
+        int i_end = ii+BLOCK<M?ii+BLOCK:M;
+        for(int jj=0 ; jj<N ; jj+=BLOCK){
+            int j_end = jj+BLOCK<N?jj+BLOCK:N;
+             for (int pp = 0; pp < K; pp += BLOCK) {
+
+                int p_end = (pp + BLOCK < K) ? pp + BLOCK : K;
+
+                //compute one block first
+                for(int i=ii; i<i_end; i++){
+                    const float* a = A+ static_cast<long>(i)*lda;
+
+                    for(int j=jj ; j<j_end; j++){
+                        const float* b = B + static_cast<long>(j)*ldb;
+
+                        float acc = C[static_cast<long>(i)*ldc + j];
+
+                        for(int p=pp ; p<p_end ; p++){
+                            acc += a[p] * b[p];
+                        }
+
+                        C[static_cast<long>(i) * ldc + j] = acc;
+
+                    }
+                }
+
+            }
+        }
+    }
+    
+
 }
