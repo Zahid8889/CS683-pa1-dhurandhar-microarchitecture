9aca3c598b0afb3d41e74b30d2f3e0cef84ca0c0
Ankit Banerjee <banerjeeankitmail@gmail.com>
2026-09-05 14:49:15 +0530

    merged file from zahid to use 256 simd instead of 512, speedup 11x to 13x

--- task2/src/matmul_simd.cpp
+++ task2/src/matmul_simd.cpp
@@ -19,28 +19,40 @@ void matmul_simd(const float *A, const float *B, float *C,
                 for (int jk = j; jk < j+4; ++jk) 
                     C[i*ldc +jk] = 0.0f;
             // float acc = 0.0f;
-            // __m512 acc = _mm512_setzero_ps();
-            __m512 s0 = _mm512_setzero_ps(), s1 = s0, s2 = s1, s3 = s2;
+            // __m256 acc = _mm256_setzero_ps();
+            __m256 s0 = _mm256_setzero_ps(), s1 = s0, s2 = s1, s3 = s2;
             // const float* b = B + static_cast<long>(j) * ldb;
             const float *b0 = B + j * ldb, *b1 = b0 + ldb, *b2 = b1 + ldb, *b3 = b2 + ldb;
             int p;
-            for (p = 0; p + 15 < K; p += 16)
+            for (p = 0; p + 7 < K; p += 8)
             {
-                // __m512 reg_a = _mm512_loadu_ps(&a[p]);
-                // __m512 reg_b = _mm512_loadu_ps(&b[p]);
-                // // __m512 reg_c = _mm512_mul_ps(reg_a,reg_b);
-                // acc = _mm512_fmadd_ps(reg_a, reg_b, acc);
-                __m512 av = _mm512_loadu_ps(a + p);
-                s0 = _mm512_fmadd_ps(av, _mm512_loadu_ps(b0 + p), s0);
-                s1 = _mm512_fmadd_ps(av, _mm512_loadu_ps(b1 + p), s1);
-                s2 = _mm512_fmadd_ps(av, _mm512_loadu_ps(b2 + p), s2);
-                s3 = _mm512_fmadd_ps(av, _mm512_loadu_ps(b3 + p), s3);
+                // __m256 reg_a = _mm256_loadu_ps(&a[p]);
+                // __m256 reg_b = _mm256_loadu_ps(&b[p]);
+                // // __m256 reg_c = _mm256_mul_ps(reg_a,reg_b);
+                // acc = _mm256_fmadd_ps(reg_a, reg_b, acc);
+                __m256 av = _mm256_loadu_ps(a + p);
+                s0 = _mm256_fmadd_ps(av, _mm256_loadu_ps(b0 + p), s0);
+                s1 = _mm256_fmadd_ps(av, _mm256_loadu_ps(b1 + p), s1);
+                s2 = _mm256_fmadd_ps(av, _mm256_loadu_ps(b2 + p), s2);
+                s3 = _mm256_fmadd_ps(av, _mm256_loadu_ps(b3 + p), s3);
             }
-            // float horizontal_sum = _mm512_reduce_add_ps(acc);
-            float c0 = _mm512_reduce_add_ps(s0);
-            float c1 = _mm512_reduce_add_ps(s1);
-            float c2 = _mm512_reduce_add_ps(s2);
-            float c3 = _mm512_reduce_add_ps(s3);
+            // float horizontal_sum = _mm256_reduce_add_ps(acc);
+            // float c0 = _mm256_reduce_add_ps(s0);
+            // float c1 = _mm256_reduce_add_ps(s1);
+            // float c2 = _mm256_reduce_add_ps(s2);
+            // float c3 = _mm256_reduce_add_ps(s3);
+            float c0=0, c1=0, c2=0, c3=0;
+            float t0[8],t1[8],t2[8],t3[8];
+            _mm256_storeu_ps(t0, s0);
+            _mm256_storeu_ps(t1, s1);
+            _mm256_storeu_ps(t2, s2);
+            _mm256_storeu_ps(t3, s3);
+            for (int q = 0; q < 8; ++q){
+                c0 += t0[q];
+                c1 += t1[q];
+                c2 += t2[q];
+                c3 += t3[q];
+            }
             for (; p < K; p++)
             {
 
